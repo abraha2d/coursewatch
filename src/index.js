78ad893e4f29991f3c40d7fbf6466d16ae81a5d4
Kevin Abraham <abraha2d@users.noreply.github.com>
2018-06-28 23:04:27 -0400

    Add redux and router and a bunch of stuff... ¯\_(ツ)_/¯

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 import './index.css';
-import App from './App';
+import App from 'containers/App';
+import configureStore from './configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create redux store with history
+const initialState = {};
+const history = createHistory();
+const store = configureStore(initialState, history);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
