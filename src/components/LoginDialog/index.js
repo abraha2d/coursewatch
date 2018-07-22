d14de4b799c9b8e13fbb982de88000bb4e3b34bc
Kevin Abraham <abraha2d@users.noreply.github.com>
2018-07-22 16:16:09 -0400

    Integrate login form with API

--- src/components/LoginDialog/index.js
+++ src/components/LoginDialog/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import logo from 'images/logo.png';
 
@@ -9,6 +10,7 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
+  Snackbar,
   TextField,
   withMobileDialog,
   withStyles
@@ -41,76 +43,114 @@ class LoginDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: false
+      username: '',
+      password: '',
+      error: false,
+      showLoginMessage: false
     };
   }
 
   handleChange(name) {
     return event => {
       this.setState({
-        [name]: event.target.value
+        [name]: event.target.value,
+        error: false
       });
     };
   }
 
-  handleSubmit() {}
+  handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post(
+        '/api/auth',
+        {
+          access_token: process.env.REACT_APP_API_MASTER_KEY
+        },
+        {
+          auth: { username: this.state.username, password: this.state.password }
+        }
+      )
+      .then(response => {
+        this.setState({ showLoginMessage: true });
+        console.log(response);
+      })
+      .catch(error => {
+        this.setState({ error: true });
+        console.error(error);
+      });
+  };
 
   render() {
     return (
-      <Dialog
-        open
-        fullScreen={this.props.fullScreen}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">
-          <img
-            className={this.props.classes.headerImage}
-            src={logo}
-            alt="Coursewatch Login"
-          />
-        </DialogTitle>
-        <DialogContent>
-          <TextField
-            id="username"
-            label="Username"
-            type="username"
-            autoComplete="username"
-            required
-            autoFocus
-            fullWidth
-            margin="dense"
-            value={this.state.username}
-            onChange={this.handleChange('username')}
-          />
-          <TextField
-            id="password"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            required
-            fullWidth
-            margin="dense"
-            value={this.state.password}
-            onChange={this.handleChange('password')}
-          />
-          <div className={this.props.classes.orSpan}>– or –</div>
-          <div
-            className={`g-signin2 ${this.props.classes.googleLogin}`}
-            data-onsuccess="onSignIn"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button>Forgot Password?</Button>
-          <Button
-            className={this.props.classes.loginButton}
-            variant="contained"
-            color="primary"
-            onClick={this.handleSubmit}
-          >
-            Login
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <div>
+        <Dialog
+          open
+          fullScreen={this.props.fullScreen}
+          aria-labelledby="form-dialog-title"
+        >
+          <form onSubmit={this.handleSubmit}>
+            <DialogTitle id="form-dialog-title">
+              <img
+                className={this.props.classes.headerImage}
+                src={logo}
+                alt="Coursewatch Login"
+              />
+            </DialogTitle>
+            <DialogContent>
+              <TextField
+                id="username"
+                label="Username"
+                type="username"
+                autoComplete="username"
+                required
+                autoFocus
+                fullWidth
+                margin="dense"
+                value={this.state.username}
+                onChange={this.handleChange('username')}
+                error={this.state.error}
+              />
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                required
+                fullWidth
+                margin="dense"
+                value={this.state.password}
+                onChange={this.handleChange('password')}
+                error={this.state.error}
+              />
+              <div className={this.props.classes.orSpan}>– or –</div>
+              <div
+                className={`g-signin2 ${this.props.classes.googleLogin}`}
+                data-onsuccess="onSignIn"
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button>Forgot Password?</Button>
+              <Button
+                className={this.props.classes.loginButton}
+                variant="contained"
+                color="primary"
+                type="submit"
+              >
+                Login
+              </Button>
+            </DialogActions>
+          </form>
+        </Dialog>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right'
+          }}
+          open={this.state.showLoginMessage}
+          message="Authentication successful!"
+        />
+      </div>
     );
   }
 }
