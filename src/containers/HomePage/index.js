0ca323ce0a2ebfdefd22a367fba1be38b787035c
Kevin Abraham <abraha2d@users.noreply.github.com>
2018-07-25 16:29:35 -0400

    Rework home page, mock up interface

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -6,97 +6,35 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 
 import { withStyles } from "@material-ui/core/styles";
 
 import {
-  Drawer,
   AppBar,
-  Toolbar,
-  List,
-  Typography,
-  IconButton,
-  Hidden,
   Divider,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
   ListItem,
   ListItemIcon,
   ListItemText,
-  ListItemAvatar,
   ListItemSecondaryAction,
-  Snackbar,
-  Button,
-  Avatar
+  MenuList,
+  MenuItem,
+  Toolbar,
+  Typography
 } from "@material-ui/core";
 
 import {
-  Menu as MenuIcon,
-  Inbox as InboxIcon,
-  Drafts as DraftsIcon,
-  Star as StarIcon,
-  Send as SendIcon,
-  Mail as MailIcon,
-  Delete as DeleteIcon,
-  Report as ReportIcon,
   Add as AddIcon,
-  Folder as FolderIcon
+  Delete as DeleteIcon,
+  Inbox as InboxIcon,
+  Menu as MenuIcon,
+  Settings as SettingsIcon
 } from "@material-ui/icons";
 
 import LogoutButton from "containers/LoginPage/ConnectedLogoutButton";
-
-const drawerWidth = 240;
-
-const mailFolderListItems = (
-  <div>
-    <ListItem button>
-      <ListItemIcon>
-        <InboxIcon />
-      </ListItemIcon>
-      <ListItemText primary="Inbox" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <StarIcon />
-      </ListItemIcon>
-      <ListItemText primary="Starred" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <SendIcon />
-      </ListItemIcon>
-      <ListItemText primary="Send mail" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <DraftsIcon />
-      </ListItemIcon>
-      <ListItemText primary="Drafts" />
-    </ListItem>
-  </div>
-);
-
-const otherMailFolderListItems = (
-  <div>
-    <ListItem button>
-      <ListItemIcon>
-        <MailIcon />
-      </ListItemIcon>
-      <ListItemText primary="All mail" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <DeleteIcon />
-      </ListItemIcon>
-      <ListItemText primary="Trash" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ReportIcon />
-      </ListItemIcon>
-      <ListItemText primary="Spam" />
-    </ListItem>
-  </div>
-);
 
 const styles = theme => ({
   root: {
@@ -117,9 +55,14 @@ const styles = theme => ({
       display: "none"
     }
   },
+  logoutAlign: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "flex-end"
+  },
   toolbar: theme.mixins.toolbar,
   drawerPaper: {
-    width: drawerWidth,
+    width: 240,
     [theme.breakpoints.up("md")]: {
       position: "relative"
     }
@@ -128,83 +71,40 @@ const styles = theme => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing.unit * 3
-  },
-  fab: {
-    position: "absolute",
-    bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 2
-  },
-  fabMoveUp: {
-    transform: "translate3d(0, -46px, 0)",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.enteringScreen,
-      easing: theme.transitions.easing.easeOut
-    })
-  },
-  fabMoveDown: {
-    transform: "translate3d(0, 0, 0)",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.leavingScreen,
-      easing: theme.transitions.easing.sharp
-    })
-  },
-  snackbar: {
-    position: "absolute"
-  },
-  snackbarContent: {
-    width: 360
-  },
-  rightAlign: {
-    flex: 1,
-    display: "flex",
-    justifyContent: "flex-end"
-  },
-  logoutButton: {
-    color: "white"
   }
 });
 
-function generate(element) {
-  return [0, 1, 2, 3, 4].map(value =>
-    React.cloneElement(element, {
-      key: value
-    })
-  );
-}
-
 class HomePage extends React.PureComponent {
   state = {
-    mobileOpen: false,
-    snackbarOpen: false
+    drawerOpen: false
   };
 
   handleDrawerToggle = () => {
-    this.setState({ mobileOpen: !this.state.mobileOpen });
-  };
-
-  handleSnackbarOpen = () => {
-    this.setState({ snackbarOpen: true });
-  };
-
-  handleSnackbarClose = () => {
-    this.setState({ snackbarOpen: false });
+    this.setState({ drawerOpen: !this.state.drawerOpen });
   };
 
   render() {
-    const { classes, theme } = this.props;
-    const { snackbarOpen } = this.state;
-    const fabClassName = classNames(
-      classes.fab,
-      snackbarOpen ? classes.fabMoveUp : classes.fabMoveDown
-    );
+    const { classes } = this.props;
 
     const drawer = (
       <div>
         <div className={classes.toolbar} />
         <Divider />
-        <List>{mailFolderListItems}</List>
-        <Divider />
-        <List>{otherMailFolderListItems}</List>
+        <MenuList>
+          <MenuItem>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Course List" />
+          </MenuItem>
+          <Divider />
+          <MenuItem>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </MenuItem>
+        </MenuList>
       </div>
     );
 
@@ -215,20 +115,17 @@ class HomePage extends React.PureComponent {
             <IconButton
               className={classes.navIconHide}
               color="inherit"
-              aria-label="open drawer"
               onClick={this.handleDrawerToggle}
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant="title" color="inherit" noWrap>
+
+            <Typography variant="title" color="inherit">
               Coursewatch
             </Typography>
 
-            <div className={classes.rightAlign}>
-              <LogoutButton
-                variant="outlined"
-                className={classes.logoutButton}
-              />
+            <div className={classes.logoutAlign}>
+              <LogoutButton variant="outlined" color="inherit" />
             </div>
           </Toolbar>
         </AppBar>
@@ -236,8 +133,7 @@ class HomePage extends React.PureComponent {
         <Hidden mdUp>
           <Drawer
             variant="temporary"
-            anchor={theme.direction === "rtl" ? "right" : "left"}
-            open={this.state.mobileOpen}
+            open={this.state.drawerOpen}
             onClose={this.handleDrawerToggle}
             classes={{
               paper: classes.drawerPaper
@@ -250,7 +146,7 @@ class HomePage extends React.PureComponent {
           </Drawer>
         </Hidden>
 
-        <Hidden smDown implementation="css">
+        <Hidden smDown>
           <Drawer
             variant="permanent"
             open
@@ -264,70 +160,70 @@ class HomePage extends React.PureComponent {
 
         <main className={classes.content}>
           <div className={classes.toolbar} />
-
-          <header className="App-header">
-            {/*<img src={logo} className="App-logo" alt="logo"/>*/}
-            <Typography variant="display1" className="App-title">
-              Welcome to React
-            </Typography>
-          </header>
-
-          <Typography variant="subheading" className="App-intro">
-            To get started, edit <code>src/containers/HomePage/index.js</code>{" "}
-            and save to reload.
-          </Typography>
-
           <List>
-            {generate(
-              <ListItem button>
-                <ListItemAvatar>
-                  <Avatar>
-                    <FolderIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Single-line item"
-                  secondary="Secondary text"
-                />
-                <ListItemSecondaryAction>
-                  <IconButton aria-label="Delete">
-                    <DeleteIcon />
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            )}
+            <ListItem button>
+              <ListItemText
+                primary="ECE 4011 - Ece Culminating Design 1"
+                secondary="CRN: 86217"
+              />
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Delete">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+            <ListItem button>
+              <ListItemText
+                primary="ECE 4180 - Embedded Systems Design"
+                secondary="CRN: 87541"
+              />
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Delete">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+            <ListItem button>
+              <ListItemText
+                primary="ECE 3550 - Feedback Control Systems"
+                secondary="CRN: 86084"
+              />
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Delete">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+            <ListItem button>
+              <ListItemText
+                primary="ID 2202 - Hist-Modern Indust Dsgn"
+                secondary="CRN: 80719"
+              />
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Delete">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+            <ListItem button>
+              <ListItemText
+                primary="VIP 4602 - Vip Project Team: Sr II"
+                secondary="CRN: 91557"
+              />
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Delete">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <AddIcon />
+              </ListItemIcon>
+              <ListItemText primary="Add a course..." />
+            </ListItem>
           </List>
         </main>
-
-        <Button
-          variant="fab"
-          color="secondary"
-          className={fabClassName}
-          onClick={this.handleSnackbarOpen}
-        >
-          <AddIcon />
-        </Button>
-
-        <Snackbar
-          open={snackbarOpen}
-          autoHideDuration={10000}
-          onClose={this.handleSnackbarClose}
-          SnackbarContentProps={{
-            "aria-describedby": "snackbar-fab-message-id",
-            className: classes.snackbarContent
-          }}
-          message={<span id="snackbar-fab-message-id">Archived</span>}
-          action={
-            <Button
-              size="small"
-              color="inherit"
-              onClick={this.handleSnackbarClose}
-            >
-              Undo
-            </Button>
-          }
-          className={classes.snackbar}
-        />
       </div>
     );
   }
