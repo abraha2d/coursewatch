f755eb9bb40c9583878d37b26c2d737a54b26395
Kevin Abraham <abraha2d@users.noreply.github.com>
2018-07-25 02:05:21 -0400

    Spiffy new homepage

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -5,16 +5,285 @@
  */
 
 import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Hidden from "@material-ui/core/Hidden";
+import Divider from "@material-ui/core/Divider";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import StarIcon from "@material-ui/icons/Star";
+import SendIcon from "@material-ui/icons/Send";
+import MailIcon from "@material-ui/icons/Mail";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ReportIcon from "@material-ui/icons/Report";
+
+import Snackbar from "@material-ui/core/Snackbar";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
 
 import LogoutButton from "containers/LoginPage/ConnectedLogoutButton";
 
-export function HomePage() {
-  return (
-    <div>
-      <h1>This is HomePage component!</h1>
-      <LogoutButton />
-    </div>
-  );
+const drawerWidth = 240;
+
+const mailFolderListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <InboxIcon />
+      </ListItemIcon>
+      <ListItemText primary="Inbox" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <StarIcon />
+      </ListItemIcon>
+      <ListItemText primary="Starred" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <SendIcon />
+      </ListItemIcon>
+      <ListItemText primary="Send mail" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <DraftsIcon />
+      </ListItemIcon>
+      <ListItemText primary="Drafts" />
+    </ListItem>
+  </div>
+);
+
+const otherMailFolderListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <MailIcon />
+      </ListItemIcon>
+      <ListItemText primary="All mail" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <DeleteIcon />
+      </ListItemIcon>
+      <ListItemText primary="Trash" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <ReportIcon />
+      </ListItemIcon>
+      <ListItemText primary="Spam" />
+    </ListItem>
+  </div>
+);
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: "100vh",
+    zIndex: 1,
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%"
+  },
+  appBar: {
+    position: "absolute",
+    zIndex: theme.zIndex.drawer + 1
+  },
+  navIconHide: {
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up("md")]: {
+      position: "relative"
+    }
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3
+  },
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing.unit * 2,
+    right: theme.spacing.unit * 2
+  },
+  fabMoveUp: {
+    transform: "translate3d(0, -46px, 0)",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.enteringScreen,
+      easing: theme.transitions.easing.easeOut
+    })
+  },
+  fabMoveDown: {
+    transform: "translate3d(0, 0, 0)",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.leavingScreen,
+      easing: theme.transitions.easing.sharp
+    })
+  },
+  snackbar: {
+    position: "absolute"
+  },
+  snackbarContent: {
+    width: 360
+  }
+});
+
+class HomePage extends React.PureComponent {
+  state = {
+    mobileOpen: false,
+    snackbarOpen: false
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  };
+
+  handleSnackbarOpen = () => {
+    this.setState({ snackbarOpen: true });
+  };
+
+  handleSnackbarClose = () => {
+    this.setState({ snackbarOpen: false });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+    const { snackbarOpen } = this.state;
+    const fabClassName = classNames(
+      classes.fab,
+      snackbarOpen ? classes.fabMoveUp : classes.fabMoveDown
+    );
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>{mailFolderListItems}</List>
+        <Divider />
+        <List>{otherMailFolderListItems}</List>
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="absolute" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              className={classes.navIconHide}
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Coursewatch
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+
+        <Hidden smDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+
+          <header className="App-header">
+            {/*<img src={logo} className="App-logo" alt="logo"/>*/}
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+
+          <p className="App-intro">
+            To get started, edit <code>src/containers/HomePage/index.js</code>{" "}
+            and save to reload.
+          </p>
+
+          <LogoutButton />
+        </main>
+
+        <Button
+          variant="fab"
+          color="secondary"
+          className={fabClassName}
+          onClick={this.handleSnackbarOpen}
+        >
+          <AddIcon />
+        </Button>
+
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={10000}
+          onClose={this.handleSnackbarClose}
+          SnackbarContentProps={{
+            "aria-describedby": "snackbar-fab-message-id",
+            className: classes.snackbarContent
+          }}
+          message={<span id="snackbar-fab-message-id">Archived</span>}
+          action={
+            <Button
+              size="small"
+              color="inherit"
+              onClick={this.handleSnackbarClose}
+            >
+              Undo
+            </Button>
+          }
+          className={classes.snackbar}
+        />
+      </div>
+    );
+  }
 }
 
-export default HomePage;
+HomePage.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(HomePage);
