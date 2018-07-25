397fb8845ef33552a1e7a6db5293903afbb9da44
Kevin Abraham <abraha2d@users.noreply.github.com>
2018-07-24 23:08:25 -0400

    Move google client id into .env file

--- src/components/LoginDialog/utils.js
+++ src/components/LoginDialog/utils.js
@@ -7,8 +7,7 @@
 const loadAuth2 = () => {
   return new Promise(cb => window.gapi.load("auth2", cb)).then(() => {
     const params = {
-      client_id:
-        "988780152592-ccs5v79t0vuhvd0t1fhko53ugfmpheo1.apps.googleusercontent.com"
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID
     };
     return window.gapi.auth2.init(params);
   });
