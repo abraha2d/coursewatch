f8faa0304f7d640cec6e00eca37b288f7ddf525d
Kevin Abraham <abraha2d@users.noreply.github.com>
2018-07-26 09:43:20 -0400

    Load subscriptions from API endpoint

--- src/containers/Subscriptions/index.js
+++ src/containers/Subscriptions/index.js
@@ -9,8 +9,12 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { compose } from "redux";
+import axios from "axios";
+
+import { withStyles } from "@material-ui/core/styles";
 
 import {
+  CircularProgress,
   IconButton,
   List,
   ListItem,
@@ -19,89 +23,134 @@ import {
   ListItemText,
   Typography
 } from "@material-ui/core";
+
 import { Add as AddIcon, Delete as DeleteIcon } from "@material-ui/icons";
+
+import makeSelectAuth from "containers/LoginPage/selectors";
 
 import injectReducer from "utils/injectReducer";
 import makeSelectSubscriptions from "./selectors";
 import reducer from "./reducer";
 
-function Subscriptions() {
-  return (
-    <div>
-      <Typography variant="title">Subscriptions</Typography>
-      <List>
-        <ListItem button>
-          <ListItemText
-            primary="ECE 4011 - Ece Culminating Design 1"
-            secondary="CRN: 86217"
-          />
-          <ListItemSecondaryAction>
-            <IconButton aria-label="Delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-        <ListItem button>
-          <ListItemText
-            primary="ECE 4180 - Embedded Systems Design"
-            secondary="CRN: 87541"
-          />
-          <ListItemSecondaryAction>
-            <IconButton aria-label="Delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-        <ListItem button>
-          <ListItemText
-            primary="ECE 3550 - Feedback Control Systems"
-            secondary="CRN: 86084"
-          />
-          <ListItemSecondaryAction>
-            <IconButton aria-label="Delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-        <ListItem button>
-          <ListItemText
-            primary="ID 2202 - Hist-Modern Indust Dsgn"
-            secondary="CRN: 80719"
-          />
-          <ListItemSecondaryAction>
-            <IconButton aria-label="Delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-        <ListItem button>
-          <ListItemText
-            primary="VIP 4602 - Vip Project Team: Sr II"
-            secondary="CRN: 91557"
-          />
-          <ListItemSecondaryAction>
-            <IconButton aria-label="Delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <AddIcon />
-          </ListItemIcon>
-          <ListItemText primary="Add a course..." />
-        </ListItem>
-      </List>
-    </div>
-  );
+const styles = theme => ({
+  centerLoading: {
+    textAlign: "center"
+  }
+});
+
+class Subscriptions extends React.PureComponent {
+  state = {
+    response: null,
+    error: false
+  };
+
+  componentWillMount() {
+    axios
+      .get("/api/subscriptions/", {
+        headers: { Authorization: `Bearer ${this.props.Auth.token}` }
+      })
+      .then(response => {
+        console.log(response);
+        this.setState({ response });
+      })
+      .catch(error => this.setState({ error }));
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Typography variant="title">Subscriptions</Typography>
+        <List className={classes.centerLoading}>
+          <ListItem button>
+            <ListItemText
+              primary="ECE 4011 - Ece Culminating Design 1"
+              secondary="CRN: 86217"
+            />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+          <ListItem button>
+            <ListItemText
+              primary="ECE 4180 - Embedded Systems Design"
+              secondary="CRN: 87541"
+            />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+          <ListItem button>
+            <ListItemText
+              primary="ECE 3550 - Feedback Control Systems"
+              secondary="CRN: 86084"
+            />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+          <ListItem button>
+            <ListItemText
+              primary="ID 2202 - Hist-Modern Indust Dsgn"
+              secondary="CRN: 80719"
+            />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+          <ListItem button>
+            <ListItemText
+              primary="VIP 4602 - Vip Project Team: Sr II"
+              secondary="CRN: 91557"
+            />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+          {this.state.response ? (
+            this.state.response.data.map(course => (
+              <ListItem button key={course.id}>
+                <ListItemText primary="" secondary={`CRN: ${course.crn}`} />
+                <ListItemSecondaryAction>
+                  <IconButton aria-label="Delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))
+          ) : (
+            <CircularProgress />
+          )}
+          <ListItem button>
+            <ListItemIcon>
+              <AddIcon />
+            </ListItemIcon>
+            <ListItemText primary="Add a course..." />
+          </ListItem>
+        </List>
+      </div>
+    );
+  }
 }
 
 Subscriptions.propTypes = {
-  dispatch: PropTypes.func.isRequired
+  dispatch: PropTypes.func.isRequired,
+  Auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = createStructuredSelector({
-  subscriptions: makeSelectSubscriptions()
+  subscriptions: makeSelectSubscriptions(),
+  Auth: makeSelectAuth()
 });
 
 const withConnect = connect(mapStateToProps);
@@ -111,4 +160,4 @@ const withReducer = injectReducer({ key: "subscriptions", reducer });
 export default compose(
   withReducer,
   withConnect
-)(Subscriptions);
+)(withStyles(styles, { withTheme: true })(Subscriptions));
